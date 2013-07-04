0b97b95c9300940ad37c514248250b09320af897
richard <richard@richard.do>
2013-07-04 12:25:43 +0100

    Login params now optional

--- examples.py
+++ examples.py
@@ -6,7 +6,7 @@ def main():
     password = 'password'
     #m = mobivate.Api(email, password)
     m = mobivate.Api()
-    m.send(originator='07855555555', recipient='447825876544', message='Hello world')
+    m.send(originator='07855555555', recipient='447801234567', message='Hello world')
 
 if __name__ == '__main__':
     main()
